3178ab20323b5d2de2b6d8f8a74b1437991a843d
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-08 21:13:03 -0500

    logic changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,19 +13,28 @@ module.exports = function(app) {
   app.post("/api/dogs", function(req, res) {
     
     var newDog = req.body;
-    var matchDog = '';
+    var newScore = newDog.scores;
     var match = '';
+    var matchPic = '';
+    var diff = 0;
 
     for (var i = 0; i < dogs.length; i++) {
-    var newInt = parseInt(newDog.q1 + newDog.q2 + newDog.q3 + newDog.q4 + newDog.q5 + newDog.q6 + newDog.q7 + newDog.q8 + newDog.q9 + newDog.q10)
-    var diff = Math.abs(parseInt(dogs[i].scores) - parseInt(newInt));
+      console.log(dogs[i].name);
+      for (var j = 0; j < 10; j++) {
+    diff += Math.abs(parseInt(dogs[i].scores[j]) - parseInt(newScore[j]));
     console.log(parseInt(diff));
-    if (diff < 3 ) {
-        var match = dogs[i];
-        matchDog.push(match);
-        res.json(matchDog);
-    }
-    
+   if (diff < 4){
+        match = dogs[i].name;
+        matchPic = dogs[i].photo;
+        res.json(match + matchPic);
+   }
+
+   else {
+     res.json("Sorry, you do not have any matches!")
+   }
+  
+
+  }
     }
   });
 }
